12cf344f6ed284bfc692bb866a31eb4f443c7300
Youngjun <byj920501@naver.com>
2025-05-16 00:11:35 +0900

    test django

--- app/app/settings.py
+++ app/app/settings.py
@@ -31,3 +31,18 @@ TEMPLATES = [{
     'OPTIONS': {'context_processors': [
         'django.template.context_processors.debug',
         'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+    ]},
+}]
+
+WSGI_APPLICATION = 'app.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+STATIC_URL = '/static/'
